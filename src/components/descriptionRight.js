9e89705c561d8f2a27e7ed19989acdabfd9dcac7
tcoquery <101638952+tcoquery@users.noreply.github.com>
2022-10-07 15:47:13 +0200

    moved links to apps from img to title

--- src/components/descriptionRight.js
+++ src/components/descriptionRight.js
@@ -1,7 +1,7 @@
 function DescriptionRight (props) {
     return (
       <div class="flex flex-col w-5/6 justify-self-start">            
-        <div class="text-2xl text-red-800 font-semibold text-left mb-6">{props.title}</div>
+        <a href={props.link} class="text-2xl text-red-800 font-semibold text-left mb-6">{props.title}</a>
         <p class="text-left text-lg border-b pb-4 border-gray-400">{props.text}</p>
         <p class="text-sm text-right text-gray-600 pt-2">{props.tags}</p>
       </div>
