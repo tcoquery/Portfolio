3508ec8d1c5228d98cdb2d3cade314f4bfe85ec2
tcoquery <101638952+tcoquery@users.noreply.github.com>
2022-10-17 11:46:41 +0200

    mobile design styling

--- src/components/descriptionRight.js
+++ src/components/descriptionRight.js
@@ -1,9 +1,9 @@
 function DescriptionRight (props) {
     return (
-      <div class="flex flex-col w-5/6 justify-self-start">            
-        <a href={props.link} class="text-2xl text-red-800 font-semibold text-left mb-6">{props.title}</a>
-        <p class="text-left text-lg border-b pb-4 border-gray-400">{props.text}</p>
-        <p class="text-sm text-right text-gray-600 pt-2">{props.tags}</p>
+      <div class="flex flex-col w-5/6 justify-self-start">        
+        <div class="text-2xl text-red-800 font-semibold text-center md:text-left mb-6">{props.title}</div>    
+        <p class="text-justify md:text-left text-lg border-b pb-4 border-gray-400">{props.text}</p>
+        <p class="text-sm text-right text-gray-600 py-2">{props.tags}</p>
       </div>
     );
   }
